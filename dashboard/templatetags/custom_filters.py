f57f0fd2a26e0479b97b13cfa908f0bebb53b8ae
vdeib <adeeb.b5000@gmail.com>
2024-10-21 00:06:40 +0300

    New Release

--- dashboard/templatetags/custom_filters.py
+++ dashboard/templatetags/custom_filters.py
@@ -17,4 +17,8 @@ def get_item(lst, index):
     return lst[int(index)] if 0 <= int(index) < len(lst) else None
 
 
+@register.filter
+def index(sequence, position):
+    return sequence[position]
+
 

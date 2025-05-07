bd3fa02972ae61bc2e11a453b5e609e89391e7e9
vdeib <adeeb.b5000@gmail.com>
2025-05-07 13:26:30 +0300

    New Push

--- TheProject/settings.py
+++ TheProject/settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = 'TheProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'Templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
